f38c55d207164b1ccb8f7b83e9a33c4d1d304caa
Aadarsh Desai <adidesai91@gmail.com>
2024-11-15 00:38:00 -0500

    Modified search box

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -82,7 +82,7 @@ export function Transactions(){
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.2} stroke="currentColor" className="size-4 text-gray-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
-                <input type="text" placeholder="Order ID or Transaction ID" className="pl-2 w-full"/>
+                <input type="text" placeholder="Order ID or Transaction ID" className="pl-2 w-full focus:outline-none active:outline-none"/>
             </div>
             <div className="flex gap-3">
                 <div className="flex border border-gray-300 gap-2 px-2 text-gray-600 rounded-md items-center">
